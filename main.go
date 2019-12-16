20ec5c2068aa16338a92dc985daf34f5858fa4c7
Serhii Chebonenko <46515985+mr-olenoid@users.noreply.github.com>
2019-12-16 17:54:32 +0200

    Update main.go
    wwwroot added

--- main.go
+++ main.go
@@ -49,6 +49,7 @@ func logout(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	wwwroot := os.Getenv("wwwroot")
 
 	router := mux.NewRouter()
 	router.HandleFunc("/hello", handler).Methods("GET")
@@ -56,15 +57,15 @@ func main() {
 	router.HandleFunc("/", login).Methods("POST")
 	router.HandleFunc("/secret", secret).Methods("GET")
 
-	staticFileDirectory := http.Dir("./assets/")
-	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
-	router.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	staticFileDirectory := http.Dir(wwwroot)
+	staticFileHandler := http.StripPrefix(wwwroot + "assets/", http.FileServer(staticFileDirectory))
+	router.PathPrefix(wwwroot + "assets/").Handler(staticFileHandler).Methods("GET")
 
 	http.ListenAndServe(":8080", router)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("./www/base.html", "./www/index.html"))
+	tmpl := template.Must(template.ParseFiles(wwwroot + "www/base.html", wwwroot + "www/index.html"))
 	data := pData{Name: "Vasya"}
 	//tmpl.ExecuteTemplate(w, "layout", data)
 	tmpl.Execute(w, data)
@@ -73,6 +74,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func loginPage(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("./www/login.html"))
+	tmpl := template.Must(template.ParseFiles(wwwroot + "www/login.html"))
 	tmpl.Execute(w, "")
 }
